c224ef57f6a722d972e402cba158ed350cc82d2c
Parth <parthgupta21112005@gmail.com>
2026-07-24 12:34:07 +0530

    deploy

---
+++ src/mycomponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero illum illo ea. Incidunt fugit quas natus eos officia doloremque, rerum asperiores veritatis cum illo nihil quos tempore cumque quidem culpa labore praesentium itaque. Voluptate.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/mycomponents/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle={
+  //   position:"relative",
+  //   top: "70vh",
+  //   width:"100%",
+  //   border: "2px solid red"
+  // }
+  return (
+    <div className='bg-dark text-light py-3'>
+      <p className="text-center">
+        Copyright &copy; MyTodoslist.com
+      </p>
+    </div>
+  )
+}
+
+export default Footer
